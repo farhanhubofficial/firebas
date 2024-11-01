78ca6e08c874f257b815c828c3fad713a1dc7867
farhanhubofficial <shaciye0757@gmail.com>
2024-11-01 19:42:30 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist' or your desired output directory
+  },
+  server: {
+    proxy: {
+      '/backend': 'http://localhost:3002', // Proxy to your backend during development
+    },
+  },
+});
